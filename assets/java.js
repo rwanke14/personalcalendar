a9f15b69245b6e8650adba0af5de6da07caa0a2f
Rachel Wanke <rkhorne09@gmail.com>
2020-12-12 23:05:00 -0500

    adding files for java, html, css and adding in beginnings of script.

--- assets/java.js
+++ assets/java.js
@@ -15,4 +15,94 @@
 
 
 
+$(document).ready(function () {
 
+
+
+    var inputBlocks = document.getElementById("inputBlcks");
+    var currentDay = document.getElementById("currentDay");
+    var hoursinDay = ["5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM", "12AM", "1AM", "2AM", "3AM", "4AM"]
+
+
+    function createBlocks(nom) {
+
+       
+
+        var calTable = $("<table>");
+        $(calTable).attr("class", "description time-block");
+
+        for (var i = 0; i < hoursinDay.length; i++) {
+           
+
+            var trEl = $("<tr>").html(hoursinDay[i]);
+            $(trEl).attr("class", "row");
+            
+            var tdElTime = $("<div>")
+            $(tdElTime).attr("class", "hour");
+
+            var tdElInput = $("<input>");
+            $(tdElInput).attr("class", "textarea");
+
+            var tdElBtn = $("<button>");
+            $(tdElBtn).attr("class", "saveBtn");
+
+
+            $(calTable).appendTo(inputBlocks);
+            $(trEl).appendTo(calTable);
+            
+            $(tdElTime).appendTo(trEl);
+
+            $(tdElInput).appendTo(trEl);
+            $(tdElBtn).appendTo(trEl);
+
+
+        }
+
+        console.log(hoursinDay)
+
+    }
+
+
+
+    createBlocks()
+
+
+    function addTimeDate () {
+
+        var date = moment().format("MM DD YYYY")
+        console.log(date);
+
+        $(currentDay).text(date);
+
+    }
+
+
+    addTimeDate()
+
+    function timeChange() {
+
+        if (s) {
+
+            //present time
+            //change color - red (call class)
+
+        } if (s) {
+
+            // past time
+            //change color - gray (present class)
+
+        } else {
+
+            //future time
+            //change color - green (future class)
+        }
+
+
+    }
+
+
+    timeChange()
+    
+    // -- add on click event for save button and add in details for local storage of the event.
+
+});
